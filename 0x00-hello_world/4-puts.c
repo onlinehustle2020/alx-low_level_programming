8c4a14638c5f24f767191a72864c1831b3a54d12
Sadiq Abdulazeez <sadiqabdulazeez2019@gmail.com>
2022-09-12 16:34:10 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the put function
+ *
+ * Description - Using the main fuction
+ *
+ * The program prints "Programming is like building a multilingual puzzle"
+ * Return 0
+ */
+
+int main();
+{
+	puts("Programming is like building a multilingual puzzle /n");
+	return 0;
+}
