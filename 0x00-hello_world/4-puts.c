bebf0e076647d130fa825f8b1ac7912a03c66ab9
Sadiq Abdulazeez <sadiqabdulazeez2019@gmail.com>
2022-09-12 23:13:11 -0700

    file update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,9 +7,8 @@
  * The program prints "Programming is like building a multilingual puzzle"
  * Return 0
  */
-
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle/n");
+	puts("/"Programming is like building a multilingual puzzle");
 	return (0);
 }
