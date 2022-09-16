333b2093949552037baa720e017a411d30268468
Sadiq Abdulazeez <sadiqabdulazeez2019@gmail.com>
2022-09-15 23:11:16 -0700

    file update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
-#include <stdio>
+#include <stdio.h>
 
 /**
  * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19", followed by new line
  *
- * Return: 0
+ * Return: 1
  */
 
 int main void
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (0);
+	return (1);
 }
