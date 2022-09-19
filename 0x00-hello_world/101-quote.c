3a1ed3d5453fab9ec72983a5578820f64b4a2b40
Sadiq Abdulazeez <sadiqabdulazeez2019@gmail.com>
2022-09-18 22:07:40 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19", followed by new line
  *
- * Return: 1
+ * Return: Always 0 success
  */
 
 int main(void)
