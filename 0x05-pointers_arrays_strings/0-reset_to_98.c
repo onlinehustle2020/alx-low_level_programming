b9f159002c3567b3328a1d426b6083f5d049f170
Sadiq Abdulazeez <sadiqabdulazeez2019@gmail.com>
2022-09-20 06:51:34 -0700

    file update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,15 +2,16 @@
 
 /**
  *
- * *reset_to_98 - resets the value of given integer to 98
+ * reset_to_98 - resets the value of given integer to 98
  *
- * *@n: parameter
+ * @n: parameter
  *
- * *Return : nothing
+ * Return : nothing
  *
- * */
+ */
 
 void reset_to_98(int *n)
 {
 		*n = 98;
 }
+
